a4c6b606ae63702f35c2ea43619146ac1cc31c7b
Fabiano De Paula <fabiano.rpaula@gmail.com>
2024-10-04 16:25:45 -0300

    Visor

---
+++ Assets/Scripts/Visor.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Visor : MonoBehaviour
+{
+    public TMP_Text Nome;
+    public TMP_Text Carne;
+    public TMP_Text Madeira;
+    public TMP_Text Ouro;
+    public TMP_Text QTDFazenderios;
+    public TMP_Text QTDCasas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
